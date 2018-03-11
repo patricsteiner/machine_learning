8b2bf7372a01fff4dae43985e2852d5101083b8c
Patric Steiner <patric.steiner1@gmail.com>
2018-03-11 16:42:33 +0100

    implement multiclass classifier with linear regression

--- ex3_multiclass_neural_network/ex3/lrCostFunction.m
+++ ex3_multiclass_neural_network/ex3/lrCostFunction.m
@@ -36,14 +36,10 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-
-
-
-
-
-
-
+h = sigmoid(theta' * X')';
+squaredThetas = theta.^2;
+J = 1/m * (-y' * log(h) - (1-y)' * log(1-h)) + lambda/(2*m) * sum(squaredThetas(2:end));
+grad = (1/m * (h-y)' * X)' + lambda/m * [0; theta(2:end)];
 
 % =============================================================
 
