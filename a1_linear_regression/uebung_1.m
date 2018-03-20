66ea053d01d787506f0dc1863be922f50fa5d671
Patric Steiner <patric.steiner1@gmail.com>
2018-03-20 09:19:52 +0100

    speed up the loading process

--- a1_linear_regression/uebung_1.m
+++ a1_linear_regression/uebung_1.m
@@ -1,11 +1,18 @@
 %% 1) Load and visualize data
-housedata = importfile('house_data.csv'); % takes a couple seconds to load all the data in a table
+% NOTE: All double quotes in the csv have been removed beforehand! (why were they there in the first place?)
+features = csvread('house_data.csv', 3, 2); % start from 3rd column (price) and 2nd row (skip header)
+labels = features(:, 1);
+features = features(:, 2:end);
+file = fopen('house_data.csv');
+feature_titles = split(replace(fgetl(file), '_', ' '), ',');
+feature_titles(1:3) = []; % remove id, date, price labels
+fclose(file);
 figure('Name', 'Comparison of house features vs. price'); % display scatterplots for all features vs. the price
-for i = 4:21 % first 3 columns are id, date and price, features start from column 4
-    subplot(4, 5, i-3);
-    scatter(housedata{:, i}, housedata.price, 0.5, 'r')
+for i = 1:length(feature_titles)
+    subplot(4, 5, i);
+    scatter(features(:, i), labels, 0.5, 'r')
     ylabel('price');
-    xlabel(housedata.Properties.VariableNames{i});
+    xlabel(feature_titles(i));
     set(gca,'FontSize', 8);
 end
 set(gcf, 'Position', [200, 200, 1000, 800]);
@@ -14,14 +21,14 @@ disp('press any key to continue...');
 pause;
 
 %% 1.5) Feature scaling (standardization) to avoid overflows and speed up gradient descent
-for i = 4:21 % first 3 columns are id, date and price, features start from column 4
-    housedata{:, i} = (housedata{:, i} - mean(housedata{:, i})) / std(housedata{:, i});
+for i = 1:size(features, 2) % first 3 columns are id, date and price, features start from column 4
+    features(:, i) = (features(:, i) - mean(features(:, i))) / std(features(:, i));
 end
 
 %% 2) Linear regression for price using sqft_living
-m = size(housedata, 1); % number of training samples
-X = [ones(m, 1), housedata.sqft_living]; % feature matrix with added column of ones
-y = housedata.price; % labels
+m = size(features, 1); % number of training samples
+X = [ones(m, 1), features(:, 3)]; % feature matrix with an added column of 1s (column 3 is sqft_living)
+y = labels;
 theta = zeros(2, 1); % initial parameters (weights)
 alpha = 0.1; % learning rate
 num_iters = 100;
@@ -49,16 +56,18 @@ hold off;
 figure;
 subplot(2, 1, 1);
 residuals = y - X * theta; % residuals = actual values - predictions
-plot(1:length(residuals), residuals, '.r', 'MarkerSize', 0.5);
+plot(X(:, 2), residuals, '.r', 'MarkerSize', 0.5);
 hold on;
-plot(1:size(X, 1), zeros(size(X, 1), 1)+1, '-g', 'LineWidth', 2);
+plot(xlim(), [0, 0], '-g', 'LineWidth', 2);
 legend('Difference to prediction', '0 line');
 title('Turkey-Anscombe plot');
-xlabel('adjusted values');
+xlabel('prediction');
 ylabel('residuals');
 hold off;
 
 subplot(2, 1, 2);
 histogram(residuals, 50);
-title('Turkey-Anscombe histogram'); % TODO labels
+title('Turkey-Anscombe histogram');
+xlabel('residuals');
+ylabel('frequency');
 
