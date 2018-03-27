d4707c8e72071ae6b3cce7a0184dc45b62862144
Patric Steiner <patric.steiner1@gmail.com>
2018-03-27 16:49:20 +0200

    implement linear regression cost function

--- ex5_bias_variance/ex5/linearRegCostFunction.m
+++ ex5_bias_variance/ex5/linearRegCostFunction.m
@@ -19,16 +19,9 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-
-
-
-
-
-
-
-
-
-
+h = X * theta;
+J = 1/(2*m) * sum((h - y).^2) + lambda/(2*m) * sum(sum(theta(2:end).^2));
+grad = (1/m * (h - y)' * X)' + lambda/m * [0; theta(2:end)];
 
 % =========================================================================
 
