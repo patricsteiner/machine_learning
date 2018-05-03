16be0ee227c9159b725058c2bedada7fa86420dc
Patric Steiner <patric.steiner1@gmail.com>
2018-05-04 00:18:22 +0200

    fisnish task2

--- a2_classification/evaluateMultiClass.m
+++ a2_classification/evaluateMultiClass.m
@@ -8,15 +8,30 @@ for n = 1:size(predicted, 1)
     j = find(predicted(n, :) == 1);
 	confusionMatrix(i, j) = confusionMatrix(i, j) + 1;
 end
-    
-tp = sum(sum(predicted & actual));
-fp = sum(sum(predicted & ~actual));
-tn = sum(sum(~predicted & ~actual));
-fn = sum(sum(~predicted & actual));
-precision = tp / (tp + fp);
-recall = tp / (tp + fn);
-accuracy = (tp + tn) / (tp + fp + tn + fn);
-fscore = 2 * (precision * recall) / (precision + recall);
+
+% calculate values per class (result is row vector)
+tp = sum(predicted & actual);
+fp = sum(predicted & ~actual);
+tn = sum(~predicted & ~actual);
+fn = sum(~predicted & actual);
+
+precision = tp ./ (tp + fp);
+recall = tp ./ (tp + fn);
+accuracy = (tp + tn) ./ (tp + fp + tn + fn);
+fscore = 2 * (precision .* recall) ./ (precision + recall);
+% make sure the averages are not messed up by potential NaNs.
+precision(isnan(fscore)) = 0; 
+recall(isnan(fscore)) = 0;
+fscore(isnan(fscore)) = 0; 
+
+% now instead of taking the mean, calculate a weighted average for all the
+% metrics, this will give more meaningful results.
+total_per_class = sum(actual);
+total = sum(total_per_class);
+precision = sum(precision .* total_per_class) / total;
+recall = sum(recall .* total_per_class) / total;
+accuracy = sum(accuracy .* total_per_class) / total;
+fscore = sum(fscore .* total_per_class) / total;
 
 if print
     fprintf('Confusion Matrix:\n');
@@ -24,7 +39,7 @@ if print
     fprintf('Accuracy:\t%.3f\n', accuracy);
     fprintf('Precision:\t%.3f\n', precision);
     fprintf('Recall: \t%.3f\n', recall);
-    fprintf('F-Score:\t%.3f\n', fscore);
+    fprintf('F-score:\t%.3f\n', fscore);
 end
     
 end
--- a2_classification/task2.m
+++ a2_classification/task2.m
@@ -18,40 +18,78 @@ xlabel('condition');
 ylabel('frequency');
 
 %% feature selection
+% set labels
+Y = zeros(size(labels_train, 1), 5);
+for i = 1:5
+    Y(:, i) = labels_train == i; % make categorical labels
+end
+Ytest = zeros(size(labels_test, 1), 5);
+for i = 1:5
+    Ytest(:, i) = labels_test == i; % make categorical labels
+end
+% feature set 1
 X = [
     normalizeFeatures(features_train(:, 1:13)), ...
     normalizeFeatures(features_train(:, 15:end)), ...
     onehotEncode(features_train(:, 14)) %zipcode
     ];
-Y = zeros(size(X, 1), 5);
-for i = 1:5
-    Y(:, i) = labels_train == i; % make categorical labels
-end
 Xtest = [
     normalizeFeatures(features_test(:, 1:13)), ...
     normalizeFeatures(features_test(:, 15:end)), ...
     onehotEncode(features_test(:, 14)) %zipcode
     ];
-Ytest = zeros(size(Xtest, 1), 5);
-for i = 1:5
-    Ytest(:, i) = labels_test == i; % make categorical labels
-end
+% feature set 2
+X = [
+    normalizeFeatures(features_train(:, 9)), ... % grade
+    normalizeFeatures(features_train(:, 12:13)), ... % year built & renovated
+    normalizeFeatures(features_train(:, 15:16)), ... % long & lat
+    onehotEncode(features_train(:, 14)) %zipcode
+    ];
+Xtest = [
+    normalizeFeatures(features_test(:, 9)), ... % grade
+    normalizeFeatures(features_test(:, 12:13)), ... % year built & renovated
+    normalizeFeatures(features_test(:, 15:16)), ... % long & lat
+    onehotEncode(features_test(:, 14)) %zipcode
+    ];
+% feature set 3 (same as 1 again, using all features)
+X = [
+    normalizeFeatures(features_train(:, 1:13)), ...
+    normalizeFeatures(features_train(:, 15:end)), ...
+    onehotEncode(features_train(:, 14)) %zipcode
+    ];
+Xtest = [
+    normalizeFeatures(features_test(:, 1:13)), ...
+    normalizeFeatures(features_test(:, 15:end)), ...
+    onehotEncode(features_test(:, 14)) %zipcode
+    ];
 
 
 %% split data
 [Xtrain, Xval, ~, Ytrain, Yval, ~] = splitDataset(X, Y, .9, .1);
 
 %% setup neural network
+% architecture 1
+inputLayer = Layer(size(X, 2), true, @linear);
+outputLayer = Layer(size(Y, 2), false, @sigmoid);
+nn = NeuralNetwork(inputLayer, outputLayer);
+nn = nn.addLayer(Layer(size(X, 2), true, @sigmoid));
+% architecture 2
+inputLayer = Layer(size(X, 2), true, @linear);
+outputLayer = Layer(size(Y, 2), false, @sigmoid);
+nn = NeuralNetwork(inputLayer, outputLayer);
+nn = nn.addLayer(Layer(size(X, 2), true, @relu)); % use relu instead of sigmoid
+% architecture 3
 inputLayer = Layer(size(X, 2), true, @linear);
 outputLayer = Layer(size(Y, 2), false, @sigmoid);
 nn = NeuralNetwork(inputLayer, outputLayer);
 nn = nn.addLayer(Layer(size(X, 2), true, @relu));
+nn = nn.addLayer(Layer(20, true, @sigmoid)); % additional sigmoid layer with 20 neurons + bias
 
 %% set parameters and train network using different lambdas
 alpha = .1; %learning rate
-n_iters = 1000;
-init_weight_min = -1; %lower bound for random weight initialization
-init_weight_max = 1; % upper bound
+%n_iters = 200;
+init_weight_min = -.1; %lower bound for random weight initialization
+init_weight_max = .1; % upper bound
 
 % lambdaValues = [0 0.0001 0.0003 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10 30 100];
 % trainLoss = [];
@@ -71,8 +109,8 @@ init_weight_max = 1; % upper bound
 % xlabel('lambda');
 % ylabel('loss');
 
-lambda = 1; %regularization parameter, choose best according to the visual evaluation above
-n_iters = 100; % potentially increase n_iters here after finding a good lambda
+lambda = .0005; %regularization parameter, choose best according to the visual evaluation above
+n_iters = 3000; % potentially increase n_iters here after finding a good lambda
 nn = nn.initWeights(init_weight_min, init_weight_max);
 nn = nn.train(Xtrain, Ytrain, alpha, lambda, n_iters, true);
 
